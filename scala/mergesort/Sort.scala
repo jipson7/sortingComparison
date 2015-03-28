c321a996de5f3f8f40e7f5a4ffed0c9ba9ea69be
jipson7 <caleb.phillips@uoit.net>
2015-03-28 19:32:45 -0400

    Switched merge sort to tail recursion to prevent Stack Overflow

--- scala/mergesort/Sort.scala
+++ scala/mergesort/Sort.scala
@@ -3,30 +3,18 @@ package mergesort {
 
   object Sort {
 
-    def MergeSort[T](list:List[T], sort:(T, T)=>Boolean):List[T] = {
-
-      val mid = list.length / 2
-
-      if (mid == 0)
-        list
-      else {
-        def merge(listA: List[T], listB: List[T], sort:(T, T)=>Boolean): List[T] = 
-          (listA, listB) match {
-            case(Nil,listB) => listB
-            case(listA, Nil) => listA
-            case(x :: listA1, y :: listB1) =>
-              if (sort(x, y)) 
-                x::merge(listA1, listB, sort)
-              else
-                y::merge(listA, listB1, sort)
-
-          }
-          val (left, right) = list splitAt(mid)
-          merge(MergeSort(left, sort), MergeSort(right, sort), sort)
-        
-
+    def MergeSort[T](xs: List[T], sort: (T, T) => Boolean): List[T] = { 
+      def merge(left: List[T], right: List[T], sort: (T, T) => Boolean): Stream[T] = (left, right) match {
+        case (x :: xs, y :: ys) if sort(x, y) => Stream.cons(x, merge(xs, right, sort))
+        case (x :: xs, y :: ys) => Stream.cons(y, merge(left, ys, sort))
+        case _ => if (left.isEmpty) right.toStream else left.toStream
       }
-      
+      val n = xs.length / 2 
+      if (n == 0) xs 
+      else { 
+        val (ys, zs) = xs splitAt n 
+        merge(MergeSort(ys, sort), MergeSort(zs, sort), sort).toList
+      } 
     }
 
   }
