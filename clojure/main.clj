a95e589df702ed4c11025ca5b86ebff2d4062e26
jipson7 <caleb.phillips@uoit.net>
2015-04-02 15:27:38 -0400

    Added string and float test to main

--- clojure/main.clj
+++ clojure/main.clj
@@ -9,8 +9,20 @@
 (load-file "./quicksort/sort.clj")
 (load-file "./mergesort/sort.clj")
 
+(println "Bubble Sort")
 (pprint (bubble-sort (list 3 4 2 5 7 1 9 8 6 0)))
+(pprint (bubble-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
+(pprint (bubble-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+(println)
 
+(println "Quick Sort")
 (pprint (quick-sort (list 3 4 2 5 7 1 9 8 6 0)))
+(pprint (quick-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
+(pprint (quick-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+(println)
 
+(println "Merge Sort")
 (pprint (merge-sort (list 3 4 2 5 7 1 9 8 6 0)))
+(pprint (merge-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
+(pprint (merge-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+
