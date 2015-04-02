e6a48a2ceed9fbd483b7e998ec836fc326778951
jipson7 <caleb.phillips@uoit.net>
2015-04-02 13:54:42 -0400

    Added working merge sort

--- clojure/main.clj
+++ clojure/main.clj
@@ -2,7 +2,10 @@
 
 (load-file "./bubblesort/sort.clj")
 (load-file "./quicksort/sort.clj")
+(load-file "./mergesort/sort.clj")
 
 (pprint (bubble-sort (list 3 4 2 5 7 1 9 8 6 0)))
 
 (pprint (quick-sort (list 3 4 2 5 7 1 9 8 6 0)))
+
+(pprint (merge-sort (list 3 4 2 5 7 1 9 8 6 0)))
---
+++ clojure/mergesort/sort.clj
@@ -0,0 +1,11 @@
+(ns sorting)
+
+(defn merge-step [list1 list2]
+  (cond
+      (nil? (first list1)) list2
+      (nil? (first list2)) list1
+      :else (if (< (first list1) (first list2))
+              (cons (first list1) (lazy-seq (merge-step (rest list1) list2)))
+              (cons (first list2) (lazy-seq (merge-step list1 (rest list2)))))))
+ 
+(defn merge-sort [the-list] (if (> (count the-list) 1) (let [[list1 list2] (split-at (quot (count the-list) 2) the-list)] (merge-step (merge-sort list1) (merge-sort list2))) the-list))
