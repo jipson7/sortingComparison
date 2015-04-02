d9db21c7e24ec0428f4e42b7421ff197a5f16e26
jipson7 <caleb.phillips@uoit.net>
2015-04-02 12:11:51 -0400

    Removed print statements from bubble sort

--- clojure/bubblesort/sort.clj
+++ clojure/bubblesort/sort.clj
@@ -1,9 +1,11 @@
-(ns bubblesort (:use clojure.pprint))
+(ns sorting)
+
+(defn test-comp [a b] (> a b))
 
 (defn bubble-swap [the-list]
   (if (or (nil? the-list) (nil? (second the-list)))
     the-list
-    (if (> (first the-list) (second the-list))
+    (if (test-comp (first the-list) (second the-list))
       (cons (second the-list) (cons (first the-list) (nthrest the-list 2)))
       (cons (first the-list) (bubble-swap (rest the-list))))))
 
@@ -13,4 +15,3 @@
     (recur (bubble-swap a-list))))
  
 
-(pprint (bubble-sort (list 3 4 2 5 7 1 9 8 6 0)))
