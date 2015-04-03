2b495fd95081290a97edf083254726a97b3d8d2f
jipson7 <caleb.phillips@uoit.net>
2015-04-02 20:04:11 -0400

    formatted main output

--- clojure/main.clj
+++ clojure/main.clj
@@ -5,24 +5,38 @@
     (>= (compare a b) 0) true
     (< (compare a b) 0) false))
 
+(def VALID-CHARS
+  (map char (range 97 123)))
+
+(defn random-char []
+  (nth VALID-CHARS (rand (count VALID-CHARS))))
+
+(defn random-str [length]
+  (apply str (take length (repeatedly random-char))))
+
 (load-file "./bubblesort/sort.clj")
 (load-file "./quicksort/sort.clj")
 (load-file "./mergesort/sort.clj")
 
+(def num-in-list 10)
+(defn rand-int-list [] (take num-in-list (repeatedly #(rand-int 100))))
+(defn rand-float-list [] (take num-in-list (repeatedly #(rand 100))))
+(defn rand-string-list [] (take num-in-list (repeatedly #(random-str 5))))
+
 (println "Bubble Sort")
-(pprint (bubble-sort (list 3 4 2 5 7 1 9 8 6 0)))
-(pprint (bubble-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
-(pprint (bubble-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+(pprint (bubble-sort (rand-int-list)))
+(pprint (bubble-sort (rand-float-list)))
+(println (bubble-sort (rand-string-list)))
 (println)
 
 (println "Quick Sort")
-(pprint (quick-sort (list 3 4 2 5 7 1 9 8 6 0)))
-(pprint (quick-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
-(pprint (quick-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+(pprint (quick-sort (rand-int-list)))
+(pprint (quick-sort (rand-float-list)))
+(println (quick-sort (rand-string-list)))
 (println)
 
 (println "Merge Sort")
-(pprint (merge-sort (list 3 4 2 5 7 1 9 8 6 0)))
-(pprint (merge-sort (list 0.3 0.4 0.2 0.5 0.7 0.1 0.9 0.8 0.6 0)))
-(pprint (merge-sort (list "g" "f" "e" "b" "a" "d" "c" "i" "h")))
+(pprint (merge-sort (rand-int-list)))
+(pprint (merge-sort (rand-float-list)))
+(println (merge-sort (rand-string-list)))
 
